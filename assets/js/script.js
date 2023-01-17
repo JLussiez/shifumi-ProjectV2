2860e4feba04c809eed099fd913605665b6adc87
Lussiez Julien <70692644+JLussiez@users.noreply.github.com>
2023-01-17 15:21:23 +0100

    changement

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-button.addEventListener('click', function(){ if (confirm("Are you sure you want to exit?")){window.close()}})
-
 let button = document.getElementById("test")
+
+button.addEventListener('click', function(){ if (confirm("Are you sure you want to exit?")){window.close()}})
 
 count = 3
 let larue = setInterval(() => {
