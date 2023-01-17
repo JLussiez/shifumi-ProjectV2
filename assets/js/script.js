c69008081b00b8fbf7727a12484319fd4e154903
Lussiez Julien <70692644+JLussiez@users.noreply.github.com>
2023-01-17 15:47:06 +0100

    timer fini

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 let button = document.getElementById("test")
+let image = document.querySelector("img")
+console.log(image)
 
-button.addEventListener('click', function(){ if (confirm("Are you sure you want to exit?")){window.close()}})
+// button.addEventListener('click', function(){ if (confirm("Are you sure you want to exit?")){window.close()}})
 
 count = 3
-let larue = setInterval(() => {
+let timer = setInterval(() => {
     console.log(count)
     count -= 1
+    if(count == 2)
+    image.src = "assets/img/2.svg"
+    if(count == 1)
+    image.src = "assets/img/1.svg"
+    if(count == 0)
+    image.src = "assets/img/fight.svg"
     if(count == -1)
-    console.log("ahahahah chatsibulidana mugiwarz")
-    if(count == -1)
-    clearInterval(larue)
+    clearInterval(timer)
 }, 1000);
 
 // let testuwu = setInterval(function(){
