39a8d2a6e181bfb2ebf5c97eed58c551fff2eac8
Hector Lavernhe <hectorlav1903@gmail.com>
2023-01-17 15:05:57 +0100

    compteur

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 let button = document.getElementById("test")
 
-button.addEventListener('click', function(){ if (confirm("Are you sure you want to exit?")){window.close()}})
+count = 3
+let larue = setInterval(() => {
+    console.log(count)
+    count -= 1
+    if(count == -1)
+    console.log("ahahahah chatsibulidana mugiwarz")
+    if(count == -1)
+    clearInterval(larue)
+}, 1000);
+
+// let testuwu = setInterval(function(){
+//     alert("neige")
+// }, 1000)
